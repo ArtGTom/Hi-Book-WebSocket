e25b755d9f199ab069fb4e95d6f3c5b7ad9689f2
ArtGTom <artgtom@gmail.com>
2020-08-25 18:44:59 -0300

    Connection with Postgres Config 1

--- knexfile.ts
+++ knexfile.ts
@@ -2,7 +2,7 @@ import path from 'path';
 
 module.exports = {
     client: 'pg',
-    connection: process.env.PG_CONNECTION_STRING,
+    connection: process.env.DATABASE_URL,
     migrations: {
         directory: path.resolve(__dirname, 'src', 'database', 'migrations')
     },
