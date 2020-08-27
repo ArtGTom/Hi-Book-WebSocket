90b33a42061a14aa31d047da8f51178b8dc35287
ArtGTom <artgtom@gmail.com>
2020-08-26 22:19:30 -0300

    DB connection fix 3

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,23 +1,8 @@
 import knex from 'knex';
 
-var db: knex<any | unknown> | null = null;
+const db = knex({
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+});
 
-if(process.env.MODE == 'production') {
-    db = knex({
-        client: 'pg',
-        connection: process.env.PG_CONNECTION_STRING
-    });
-} 
-else {
-    db = knex({
-        client: 'pg',
-        connection: {
-            host: 'localhost',
-            port: 5432,
-            user: 'postgres',
-            password: 'joaobanco',
-            database: 'db_hibook'
-        }
-    })
-}
-export default db as knex<any | unknown>;
+export default db;
