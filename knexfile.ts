cd6f8b1d88082aaab1a2407131fda644a7431492
ArtGTom <artgtom@gmail.com>
2020-08-27 15:36:43 -0300

    Server connection fix 6

--- knexfile.ts
+++ knexfile.ts
@@ -4,7 +4,7 @@ import path from 'path';
 module.exports = {
     production: {
         client: 'pg',
-        connection: process.env.PG_CONNECTION_STRING,
+        connection: process.env.DATABASE_URL,
         migrations: {
             directory: path.resolve(__dirname, 'src', 'database', 'migrations')
         },
